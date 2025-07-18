ee49a90e9ad29b86dbdeeb523ed65ca0acb6e4c0
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2025-07-18 21:49:48 +0430

    one layer simple map added to gui

--- windows/main_window.py
+++ windows/main_window.py
@@ -2,12 +2,15 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import QFile, QTextStream
 from widgets.map_widget import MapWidget
+from ui.MainWindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
+# class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         loadUi("ui/main_window.ui", self)
+        # self.setupUi(self)
 
         self.setWindowTitle("Python Map GUI")
         self.apply_styles()
@@ -25,8 +28,7 @@ class MainWindow(QMainWindow):
         self.mapLayout.addWidget(self.map_widget)
 
         # Connect menu actions
-        self.actionOpenLocalTiles.triggered.connect(self.open_local_tiles)
-        self.actionExit.triggered.connect(self.close)
+        self.actionImportLocalTiles.triggered.connect(self.open_local_tiles)
 
     def open_local_tiles(self):
         folder = QFileDialog.getExistingDirectory(self, "Select Tile Folder")
