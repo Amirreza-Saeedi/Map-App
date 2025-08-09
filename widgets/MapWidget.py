86cdbc1a02a2680dc72a7e7d7ac477784f546a75
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2025-08-09 22:07:21 +0430

    local map has bug...

--- widgets/MapWidget.py
+++ widgets/MapWidget.py
@@ -4,6 +4,8 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 import folium
 from folium import WmsTileLayer, TileLayer
 from folium.plugins import Draw
+import xyzservices
+
 
 # from pyqtlet2 import L, MapWidget as LeafletMap
 
@@ -49,20 +51,44 @@ class MapWidget(QWidget):
         self.setLayout(self.layout)
 
     def init_map(self):
-        map = folium.Map(tiles=None)
+        # Create custom provider for local tiles
+        custom_provider = xyzservices.Bunch(
+            name="Local Tiles",
+            url="file:///D:/Robotic Perceptoin Lab/map-tile-caching/src/outputs/tiles/{z}/{x}/{y}.png",
+            attribution="Your attribution text",
+            max_zoom=19,
+            min_zoom=17
+        )
+        map = folium.Map(
+            location=[37.453393341443174, 49.087650948025875],  # You need to specify a location
+            tiles=custom_provider.url,
+            attr=custom_provider.attribution,
+            zoom_start=17,
+            min_zoom=17,
+            max_zoom=19
+        )
 
         # online tile servers
         # DEM tiles
         [tms.add_to(map) for tms in MapWidget.TMS]
         [wms.add_to(map) for wms in MapWidget.WMS]
         # local tile directory
-        self.local_tiles = TileLayer()
+        self.local_tiles = TileLayer(
+            name="Local Tiles",
+            tiles="file:///D:/Robotic Perceptoin Lab/map-tile-caching/src/outputs/tiles/{z}/{x}/{y}.png",
+            attr="Your attribution text",
+            max_zoom=19,
+            min_zoom=17,
+            overlay=True,
+            show=False
+        )
+        self.local_tiles.add_to(map)
 
         folium.LayerControl().add_to(map)
         return map
 
     def set_local_tiles(self, path: str):
-        """kljfds
+        """
         
         Parameters:
         ---
